e6467cd7dd84ecf0203fc347940b4fa0f75e6a01
Laura Bazaldua <laura_b_m@hotmail.com>
2021-11-25 00:54:49 -0600

    Prueba de controller

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -2,10 +2,14 @@ package com.example.herokudemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "*")
+
 public class IndexController {
     @GetMapping("/")
+
     public String index() {
         return "Hola mundo.";
     }
